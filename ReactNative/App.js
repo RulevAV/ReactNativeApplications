15fc1f83a8f7f4259a41efb468fd89c34ef59850
RulevAV <rulandrei99@mail.ru>
2022-01-19 01:44:23 +0700

    opdate

--- ReactNative/App.js
+++ ReactNative/App.js
@@ -5,51 +5,45 @@ import Header from './assets/Components/Header';
 import ListItem from './assets/Components/ListItem';
 import Form from './assets/Components/Form';
 export default function App() {
-  const [listOfItems,setListOfItems] = useState([
-    {text:"Купить молоко", key:"1"},
-    {text:"Помыть машину", key:"2"},
-    {text:"Купить картошку", key:"3"},
-    {text:"Стать миллионером  ", key:"4"}
+  const [listOfItems, setListOfItems] = useState([
+    { text: "Купить молоко", key: "1" },
+    { text: "Помыть машину", key: "2" },
+    { text: "Купить картошку", key: "3" },
+    { text: "Стать миллионером  ", key: "4" }
   ]);
-  const addHeadler = (text)=>{
-    setListOfItems((list)=>{
+  const addHeadler = (text) => {
+    setListOfItems((list) => {
       return [
-        {text:text, key:Math.random().toString(36).substring(7)},
+        { text: text, key: Math.random().toString(36).substring(7) },
         ...list,
       ];
     });
   }
-  const deleteHeandler = (key)=>{
+  const deleteHeandler = (key) => {
     console.log(key);
-setListOfItems((list)=>{
-  return list.filter(listOfItems => listOfItems.key!=key)
-});
+    setListOfItems((list) => {
+      return list.filter(listOfItems => listOfItems.key != key)
+    });
   }
   return (
     <View style={styles.main}>
-      <View  style={styles.header}>
-      <Header/>
-    <Form addHeadler={addHeadler}/>
-      </View>
-      <View  style={styles.content}>
-          <FlatList data={listOfItems} renderItem={({item})=>(
-            <ListItem el={item} deleteHeandler={deleteHeandler}/>
-          )}/>
+      <Header />
+      <Form addHeadler={addHeadler} />
+      <View style={styles.content}>
+        <FlatList data={listOfItems} renderItem={({ item }) => (
+          <ListItem el={item} deleteHeandler={deleteHeandler} />
+        )} />
       </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
-  main:{
-    flex:1,
-    backgroundColor:"yellow"
+  main: {
+    flex: 1,
+    backgroundColor: "yellow"
   },
-  header:{
-    flex:1,
-  },
-  content:{
-    flex:3,
+  content: {
+    flex: 1,
   }
-
 });
 
